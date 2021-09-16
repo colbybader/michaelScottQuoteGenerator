4affbd2377714e51ab26a7b66cb4a153eee0b72c
Colby Bader <colbybader@gmail.com>
2021-09-16 16:55:34 -0700

    localQuotes()

---
+++ quotes.js
@@ -0,0 +1,102 @@
+const localQuotes = [
+    {
+        text: 'You miss 100% of the shots you don’t take. - Wayne Gretzky” - Michael Scott',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'That has sort of an oak-y afterbirth.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Wikipedia is the best thing ever. Anyone in the world can write anything they want about any subject so you know you are getting the best possible information.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I learned a while back that if I do not text 911, people do not return my calls. Um, but people always return my calls because they think that something horrible has happened.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I’m an early bird and I’m a night owl so I’m wise and I have worms.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I… Declare…. Bankruptcy!',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'It is St. Patricks Day. And here in Scranton, that is a huge deal. It is the closest that the Irish will ever get to Christmas.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'It’s Britney, bitch.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Where are the turtles?!',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'You dont know me, youve just seen my penis.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'The worst thing about prison was the Dementors. They were flying all over the place and they were scary and theyd come down and theyd suck the soul out of your body and it hurt!',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'And I knew exactly what to do. But in a much more real sense, I had no idea what to do.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I feel like all my kids grew up and then they married each other. It’s every parent’s dream.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I love inside jokes. I hope to be a part of one someday.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'If I had a gun with two bullets and I was in a room with Hitler, Bin Laden, and Toby, I would shoot Toby twice.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Society teaches us that having feelings and crying is bad and wrong. Well, thats baloney, because grief isnt wrong. Theres such a thing as good grief. Just ask Charlie Brown.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'You know what they say. Fool me once, strike one, but fool me twice...strike three.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'You cheated on me?....When I specifically asked you not to?',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Im not superstitious, but I am a little stitious.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Sometimes Ill start a sentence and I dont even know where its going. I just hope I find it along the way.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I am Beyoncé, always.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'I have cause. It is beCAUSE I hate him.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.',
+        author: 'Michael Scott',
+    },
+    {
+        text: 'That’s what she said.',
+        author: 'Michael Scott',
+    }
+]
---
+++ script.js
@@ -0,0 +1,23 @@
+//let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+   //Pick a random quote from apiQuotes array
+   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+   console.log(quote);
+}
+
+// Get Quotes From Api
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//        const response = await fetch(apiUrl);
+//        apiQuotes = await response.json();
+//        newQuote();
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
+// On Load
+// getQuotes();
